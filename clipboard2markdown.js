dc0238b304be479f57d8db0b901e1f8ec758c11c
Sampson <jjdsampson@gmail.com>
2022-08-25 14:28:53 -0500

    Various changes to base project

--- clipboard2markdown.js
+++ clipboard2markdown.js
@@ -1,186 +1,148 @@
 (function () {
-  'use strict';
 
-  // http://pandoc.org/README.html#pandocs-markdown
-  var pandoc = [
+  const isBold = node =>
+    /^(STRONG|B)$/.test( node.tagName )
+    || node.style?.fontWeight > 400;
+
+  const isItalic = node =>
+    /^(EM|I|CITE|VAR)$/.test( node.tagName )
+    || node.style?.fontStyle == 'italic';
+
+  const converters = [
     {
-      filter: 'h1',
-      replacement: function (content, node) {
-        var underline = Array(content.length + 1).join('=');
-        return '\n\n' + content + '\n' + underline + '\n\n';
+      filter: node => /^H[1-6]$/.test( node.tagName ),
+      replacement: (text, node) =>
+        `${ '#'.repeat( node.tagName[1] ) } ${text}\n\n`
+    },
+    {
+      filter: node =>
+        node.textContent
+        && ( isBold(node) || isItalic(node) ),
+      replacement: (text, node) => {
+        if ( isBold( node ) ) text = `__${text}__`;
+        if ( isItalic( node ) ) text = `*${text}*`;
+        return text;
       }
     },
-
     {
-      filter: 'h2',
-      replacement: function (content, node) {
-        var underline = Array(content.length + 1).join('-');
-        return '\n\n' + content + '\n' + underline + '\n\n';
-      }
+      filter: ['sub', 'sup'],
+      replacement: (text, node) =>
+        node.tagName === 'sub' ? `~${text}~` : `^${text}^`
     },
-
     {
-      filter: 'sup',
-      replacement: function (content) {
-        return '^' + content + '^';
-      }
+      filter: ['hr', 'br'],
+      replacement: (_, node) =>
+        node.tagName === 'HR' ? `\n\n* * * * *\n\n` : `\n`
     },
-
     {
-      filter: 'sub',
-      replacement: function (content) {
-        return '~' + content + '~';
-      }
-    },
-
-    {
-      filter: 'br',
-      replacement: function () {
-        return '\\\n';
-      }
-    },
-
-    {
-      filter: 'hr',
-      replacement: function () {
-        return '\n\n* * * * *\n\n';
-      }
-    },
-
-    {
-      filter: ['em', 'i', 'cite', 'var'],
-      replacement: function (content) {
-        return '*' + content + '*';
-      }
-    },
-
-    {
-      filter: function (node) {
-        var hasSiblings = node.previousSibling || node.nextSibling;
-        var isCodeBlock = node.parentNode.nodeName === 'PRE' && !hasSiblings;
-        var isCodeElem = node.nodeName === 'CODE' ||
-            node.nodeName === 'KBD' ||
-            node.nodeName === 'SAMP' ||
-            node.nodeName === 'TT';
+      filter: node => {
+        const hasSiblings = node.previousSibling || node.nextSibling;
+        const isCodeBlock = node.parentNode.tagName === 'PRE' && !hasSiblings;
+        const isCodeElem = /^(CODE|KBD|SAMP|TT)$/.test( node.tagName );
 
         return isCodeElem && !isCodeBlock;
       },
-      replacement: function (content) {
-        return '`' + content + '`';
-      }
+      replacement: text => `\`${text}\``
     },
-
     {
-      filter: function (node) {
-        return node.nodeName === 'A' && node.getAttribute('href');
-      },
-      replacement: function (content, node) {
-        var url = node.getAttribute('href');
-        var titlePart = node.title ? ' "' + node.title + '"' : '';
-        if (content === url) {
-          return '<' + url + '>';
-        } else if (url === ('mailto:' + content)) {
-          return '<' + content + '>';
-        } else {
-          return '[' + content + '](' + url + titlePart + ')';
+      filter: (node) =>
+        node.tagName === 'A'
+        && node.getAttribute('href'),
+      replacement: (text, node) => {
+
+        let replacement;
+        let url = node.getAttribute('href');
+        const lead = text.match(/^\s+/)?.[0] || [''];
+        const trail = text.match(/\s+$/)?.[0] || [''];
+        const title = node.title ? ` "${node.title}"` : '';
+
+        if ( lead || trail ) {
+          text = text.trim();
         }
+
+        if (url === text) {
+          replacement = `<${url}>`;
+        } else if (url === `mailto:${text}`) {
+          replacement = `<${text}>`;
+        } else {
+          replacement = `[${text}](${url}${title})`;
+        }
+
+        /**
+         * Move leading/trailing spaces out of content
+         * ✖ please [download ](https://brave.com)Brave
+         * ✖ please[ download](https://brave.com) Brave
+         * ✖ please[ download ](https://brave.com)Brave
+         * ✔ please [download](https://brave.com) Brave
+         */
+        replacement = `${ lead }${ replacement }${ trail }`;
+
+        return replacement;
+
       }
     },
-
     {
       filter: 'li',
-      replacement: function (content, node) {
+      replacement: (content, node) => {
         content = content.replace(/^\s+/, '').replace(/\n/gm, '\n    ');
-        var prefix = '-   ';
-        var parent = node.parentNode;
-
-        if (/ol/i.test(parent.nodeName)) {
-          var index = Array.prototype.indexOf.call(parent.children, node) + 1;
-          prefix = index + '. ';
-          while (prefix.length < 4) {
-            prefix += ' ';
-          }
+        let prefix = '-   ';
+        const parent = node.parentNode;
+        if ("OL" == parent.tagName) {
+          const index = [...parent.children].indexOf(node) + 1;
+          prefix = `${index}. `.padEnd(4, ' ');
         }
-
         return prefix + content;
       }
     }
   ];
 
-  // http://pandoc.org/README.html#smart-punctuation
-  var escape = function (str) {
-    return str.replace(/[\u2018\u2019\u00b4]/g, "'")
-              .replace(/[\u201c\u201d\u2033]/g, '"')
-              .replace(/[\u2212\u2022\u00b7\u25aa]/g, '-')
-              .replace(/[\u2013\u2015]/g, '--')
-              .replace(/\u2014/g, '---')
-              .replace(/\u2026/g, '...')
-              .replace(/[ ]+\n/g, '\n')
-              .replace(/\s*\\\n/g, '\\\n')
-              .replace(/\s*\\\n\s*\\\n/g, '\n\n')
-              .replace(/\s*\\\n\n/g, '\n\n')
-              .replace(/\n-\n/g, '\n')
-              .replace(/\n\n\s*\\\n/g, '\n\n')
-              .replace(/\n\n\n*/g, '\n\n')
-              .replace(/[ ]+$/gm, '')
-              .replace(/^\s+|[\s\\]+$/g, '');
-  };
+  const escape = str =>
+    str.replace(/[\u2018\u2019\u00b4]/g, "'")
+       .replace(/[\u201c\u201d\u2033]/g, '"')
+       .replace(/[\u2212\u2022\u00b7\u25aa]/g, '-')
+       .replace(/[\u2013\u2015]/g, '--')
+       .replace(/\u2014/g, '---')
+       .replace(/\u2026/g, '...')
+       .replace(/[ ]+\n/g, '\n')
+       .replace(/\s*\\\n/g, '\\\n')
+       .replace(/\s*\\\n\s*\\\n/g, '\n\n')
+       .replace(/\s*\\\n\n/g, '\n\n')
+       .replace(/\n-\n/g, '\n')
+       .replace(/\n\n\s*\\\n/g, '\n\n')
+       .replace(/\n\n\n*/g, '\n\n')
+       .replace(/[ ]+$/gm, '')
+       .replace(/^\s+|[\s\\]+$/g, '');
 
-  var convert = function (str) {
-    return escape(toMarkdown(str, { converters: pandoc, gfm: true }));
-  }
+  const convert = str =>
+    escape( toMarkdown( str, { converters, gfm: true } ) );
 
-  var insert = function (myField, myValue) {
-      if (document.selection) {
-          myField.focus();
-          sel = document.selection.createRange();
-          sel.text = myValue;
-          sel.select()
-      } else {
-          if (myField.selectionStart || myField.selectionStart == "0") {
-              var startPos = myField.selectionStart;
-              var endPos = myField.selectionEnd;
-              var beforeValue = myField.value.substring(0, startPos);
-              var afterValue = myField.value.substring(endPos, myField.value.length);
-              myField.value = beforeValue + myValue + afterValue;
-              myField.selectionStart = startPos + myValue.length;
-              myField.selectionEnd = startPos + myValue.length;
-              myField.focus()
-          } else {
-              myField.value += myValue;
-              myField.focus()
-          }
-      }
-  };
+  document.addEventListener('DOMContentLoaded', () => {
 
-  // http://stackoverflow.com/questions/2176861/javascript-get-clipboard-data-on-paste-event-cross-browser
-  document.addEventListener('DOMContentLoaded', function () {
-    var info = document.querySelector('#info');
-    var pastebin = document.querySelector('#pastebin');
-    var output = document.querySelector('#output');
-    var wrapper = document.querySelector('#wrapper');
+    const pastebin = document.querySelector('#pastebin');
+    const output = document.querySelector('#output');
 
-    document.addEventListener('keydown', function (event) {
-      if (event.ctrlKey || event.metaKey) {
-        if (String.fromCharCode(event.which).toLowerCase() === 'v') {
+    document.addEventListener('keydown', ({ key, ctrlKey, metaKey }) => {
+      if (( ctrlKey || metaKey ) && key === 'v') {
           pastebin.innerHTML = '';
           pastebin.focus();
-          info.classList.add('hidden');
-          wrapper.classList.add('hidden');
-        }
       }
     });
 
-    pastebin.addEventListener('paste', function () {
-      setTimeout(function () {
-        var html = pastebin.innerHTML;
-        var markdown = convert(html);
-        // output.value = markdown;
-        insert(output, markdown);
-        wrapper.classList.remove('hidden');
+    /**
+     * For notes on setTimeout(fn, 0), see
+     * https://developer.mozilla.org/en-US/docs/Web/API/Window/setImmediate#notes,
+     * and https://stackoverflow.com/a/779785/54680.
+     */
+    pastebin.addEventListener('paste', () => 
+      setTimeout(() => {
+        const html = pastebin.innerHTML;
+        const markdown = convert(html);
+        output.textContent = markdown;
         output.focus();
         output.select();
-      }, 200);
-    });
+      }, 0)
+    );
+
   });
+
 })();
